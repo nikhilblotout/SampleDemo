d4ce4aef5d3239037c063c5c143bc0f174091fa1
nikhil <nikhil.thangellapally@blotout.io>
2021-11-10 14:32:12 +0530

    finally done
    done with crud operations on sample list using spring boot

--- sampleProject/src/main/java/com/blotout/io/sampleProject/Controller/Controller.java
+++ sampleProject/src/main/java/com/blotout/io/sampleProject/Controller/Controller.java
@@ -2,6 +2,7 @@ package com.blotout.io.sampleProject.Controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,37 +18,37 @@ public class Controller {
 	
 	@Autowired
 	private StudentService studentService;
-
+	//greeting request
 	@RequestMapping("/hello")
 	public String greeting()
 	{
-		return "Hello world";
+		return "Hello world ";
 	}
-	
+	// get request for to retrieve all students
 	@RequestMapping("/getAll")
 	public List<Student> getAll()
 	{
 		return studentService.getStudents();
 	}
-	
+	//get request to retrieve particular student details
 	@RequestMapping("/get/{id}")
 	public Student getStudent(@PathVariable int id)
 	{
 		return studentService.getStudent(id);
 	}
-	
+	//post request to add student into list
 	@RequestMapping(method=RequestMethod.POST,value="/add")
 	public void addStudent(@RequestBody Student s)
 	{
 		studentService.addStudent(s);
 	}
-	
+	//update request to update details of student
 	@RequestMapping(method=RequestMethod.PUT,value="/update")
 	public void updateStudent(@RequestBody Student s)
 	{
 		studentService.updateStudent(s);
 	}
-	
+	//delete request for particular student based on id
 	@RequestMapping(method=RequestMethod.DELETE,value="/delete/{id}")
 	public void deleteStudent(@PathVariable int id)
 	{
